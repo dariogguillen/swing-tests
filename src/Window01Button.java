30a94232c62bfc9729b57bc0e972b9ef1f1c3ccd
dariogguillen <guillendario@gmail.com>
2025-02-24 15:30:32 -0600

    action listener as static class

--- src/Window01Button.java
+++ src/Window01Button.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Window01Button extends JFrame {
     public Window01Button() {
@@ -9,9 +12,18 @@ public class Window01Button extends JFrame {
         JButton button = new JButton("Click me!");
         button.setSize(100, 50);
         button.addActionListener(e -> System.out.println("Button clicked!"));
+        button.addActionListener(new BActionListener());
         getContentPane().add(button);
     }
     public static void main(String[] args) {
         new Window01Button();
+    }
+
+    private static class BActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            System.out.println("Button clicked2!");
+            Toolkit.getDefaultToolkit().beep();
+        }
     }
 }
