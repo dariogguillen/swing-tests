4f7ab9cfb9a68aeaa575245ae490b67e13cc0ed5
dariogguillen <guillendario@gmail.com>
2025-02-24 15:19:58 -0600

    action listener as lambda

--- src/Window01Button.java
+++ src/Window01Button.java
@@ -1,8 +1,6 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class Window01Button extends JFrame implements ActionListener {
+public class Window01Button extends JFrame {
     public Window01Button() {
         super("Button Window");
         setSize(200, 100);
@@ -10,15 +8,10 @@ public class Window01Button extends JFrame implements ActionListener {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         JButton button = new JButton("Click me!");
         button.setSize(100, 50);
-        button.addActionListener(this);
+        button.addActionListener(e -> System.out.println("Button clicked!"));
         getContentPane().add(button);
     }
     public static void main(String[] args) {
         new Window01Button();
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent actionEvent) {
-        System.out.println("Button clicked!");
     }
 }
